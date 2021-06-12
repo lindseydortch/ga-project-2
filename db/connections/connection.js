6f6b07171a9fae8f6b802d469cd67f8fa444a8c5
lindseydortch <codewithlinds@gmail.com>
2021-06-12 13:43:30 -0500

    initial commit

---
+++ db/connections/connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const mongoURI =
+process.env.NODE_ENV === 'production'
+? process.send.DB_URL
+: 'mongodb://localhost/project-2'
+
+mongoose
+.connect(mongoURI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+})
+
+.then((instance) =>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+  )
+  .catch((error) => console.log('Connection failed!', error));
+
+module.exports = mongoose;
