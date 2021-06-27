4ab26104e7256ffa48d8ae3e133c6bb8449944b4
lindseydortch <codewithlinds@gmail.com>
2021-06-27 00:40:44 -0500

    updated for deployment

--- db/connections/connection.js
+++ db/connections/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const mongoURI =
 process.env.NODE_ENV === 'production'
-? process.send.DB_URL
+? process.env.DB_URL
 : 'mongodb://localhost/project-2'
 
 mongoose
